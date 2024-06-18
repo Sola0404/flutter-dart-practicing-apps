fac67801dbc9a7c2ed1ba852f1b2dbc84842d538
Yujia <0404sola@gmail.com>
2024-06-18 12:02:24 +1000

    Change text style

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,15 +6,17 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 81, 26, 176),
-                Color.fromARGB(255, 157, 112, 234),
-              ],
-            ),
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 81, 26, 176),
+              Color.fromARGB(255, 157, 112, 234),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
           child: const Center(
-            child: Text('Hello World'),
+            child: Text("Hello World",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold)),
           ),
         ),
       ),
