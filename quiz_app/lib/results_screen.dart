f4765ea7e433d8b0a18264af97b8a43b6afd0247
Yujia <0404sola@gmail.com>
2024-06-22 18:11:48 +1000

    Update quiz app

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:quiz_app/questions_summary.dart';
 import 'package:quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
@@ -26,6 +26,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data["correct_answer"] == data["user_answer"];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -33,11 +39,12 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("You answered X out of Y questions correctly."),
+            Text(
+                "You answered $numCorrectQuestions out of $numTotalQuestions questions correctly."),
             const SizedBox(
               height: 30,
             ),
-            const Text("List of answers and questions..."),
+            QuestionsSummary(summaryData),
             const SizedBox(
               height: 30,
             ),
