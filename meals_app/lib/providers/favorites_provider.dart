c3c1e8fa6ffcfe8f6e686ffe61b28ef77b7b034f
Yujia <0404sola@gmail.com>
2024-07-01 17:42:56 +1000

    feat: Refactor FavoritesProvider to use StateNotifier

---
+++ meals_app/lib/providers/favorites_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meals_app/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  bool toggleMealFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+}
+
+// StateNotifierProvider is a provider that allows to listen to changes to a value and rebuild dependents when the value changes.
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
