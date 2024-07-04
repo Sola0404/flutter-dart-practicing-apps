423ca3ef332973fbf02e6f51c9d36c8c5ce7a942
Yujia <0404sola@gmail.com>
2024-07-04 13:00:32 +1000

    chore: Add uuid package for generating unique IDs in Place model

--- favorite_places_app/lib/main.dart
+++ favorite_places_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places_app/screens/places.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -26,19 +27,19 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+    const MyApp(),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Great Places',
       theme: theme,
-      home: ...,
+      home: const PlacesScreen(),
     );
   }
 }
